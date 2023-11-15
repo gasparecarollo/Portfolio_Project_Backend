8a3b5f6dc6e82797c0fbb1e69ddad6fd15761e82
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-15 00:44:54 -0500

    Added menuItems query

--- queries/menuItems.js
+++ queries/menuItems.js
@@ -4,18 +4,18 @@ const getAllMenuItems = async () => {
 
     try {
         const allMenuItems = await db.any("SELECT * FROM menuItems");
-        return allMenuItems
+        return allMenuItems;
     } catch (error) {
-        return error
+        return error;
     };
 }
 
 const getMenuItem = async (id) => {
     try {
         const oneMenuItem = await db.one("SELECT * FROM menuItems WHERE id = $1", id);
-        return oneMenuItem
+        return oneMenuItem;
     } catch (error) {
-        return error
+        return error;
     };
 }
 
@@ -24,9 +24,31 @@ const createMenuItem = async (menuItem) => {
 
         const newMenuItem = await db.one("INSERT INTO menuItems (name, category, image_id, description, price, out_of_stock, ranking) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *", [menuItem.name, menuItem.category, menuItem.image_id, menuItem.description, menuItem.price, menuItem.out_of_stock, menuItem.ranking]
         )
-        return newMenuItem
+        return newMenuItem;
     } catch (error) {
-        return error
+        return error;
     }
 
 }
+
+const deleteMenuItem = async (id) => {
+    try {
+        const deletedMenuItem = await db.one("DELETE FROM menuItems WHERE ID = $1 RETURNING *", id)
+        return deletedMenuItem;
+    } catch (error) {
+        return error;
+    }
+}
+
+const updateMenuItem = async (id, menuItem) => {
+    try {
+        const updatedMenuItem = await db.one("UPDATE menuItems SET name=$1, category=$2, image_id=$3, description=$4, price = $5, out_of_stock = $6, ranking = $7 WHERE ID = $8 RETURNING *", [menuItem.name, menuItem.category, menuItem.image_id, menuItem.description, menuItem.price, menuItem.out_of_stock, menuItem.ranking, id])
+
+        return updatedMenuItem;
+
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = { getAllMenuItems, getMenuItem, createMenuItem, deleteMenuItem, updateMenuItem };
