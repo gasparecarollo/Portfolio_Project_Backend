c82cfd43687ff8655f90e0947b75dd95a14e97e2
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-15 00:54:30 -0500

    Imported Morgan into app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan");
 const menuItemController = require("./controllers/menuItemController");
 
 
