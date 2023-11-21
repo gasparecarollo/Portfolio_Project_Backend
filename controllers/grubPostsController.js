d68523daf541503c87157da1f2ec6c0008a797b5
Gaspare Carollo <gasparecarollo@pursuit.org>
2023-11-20 22:57:24 -0500

    Added grubPostsController.js

--- controllers/grubPostsController.js
+++ controllers/grubPostsController.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const grubposts = express.Router();
 const tweetsController = require("./tweetsController");
-menuItems.use("/:menuItems_id/tweets", tweetsController);
 
 //QUERIES
 const {
@@ -13,23 +12,12 @@ const {
     updateGrubPost,
 } = require("../queries/grubposts");
 
-//VALIDATIONS
-const {
-    checkName,
-    checkImage_id,
-    checkCategory,
-    checkDescription,
-    checkPrice,
-    checkStock,
-    checkRanking
-} = require("../validations/checkMenuItems");
-
 //INDEX
-menuItems.get("/", async (req, res) => {
-    const allMenuItems = await getAllMenuItems();
+grubposts.get("/", async (req, res) => {
+    const allGrubPosts = await getAllGrubPosts();
 
-    if (allMenuItems[0]) {
-        res.status(200).json(allMenuItems);
+    if (allGrubPosts[0]) {
+        res.status(200).json(allGrubPosts);
 
     } else {
 
@@ -39,21 +27,21 @@ menuItems.get("/", async (req, res) => {
 });
 
 //SHOW
-menuItems.get("/:id", async (req, res) => {
+grubposts.get("/:id", async (req, res) => {
     const { id } = req.params;
-    const menuItem = await getMenuItem(id);
-    if (menuItem) {
-        res.json(menuItem);
+    const grubpost = await getGrubPost(id);
+    if (grubpost) {
+        res.json(grubpost);
     } else {
         res.status(404).json({ error: "Not Found" });
     }
 });
 
 //CREATE
-menuItems.post("/", checkName, checkImage_id, checkCategory, checkDescription, checkPrice, checkStock, checkRanking, async (req, res) => {
+grubposts.post("/", async (req, res) => {
     try {
-        const menuItem = await createMenuItem(req.body);
-        res.json(menuItem)
+        const grubpost = await createGrubPost(req.body);
+        res.json(grubpost);
     } catch (error) {
         res.status(400).json({ error: error });
     }
@@ -61,23 +49,23 @@ menuItems.post("/", checkName, checkImage_id, checkCategory, checkDescription, c
 
 
 //UPDATE
-menuItems.put("/:id", checkName, checkCategory, checkImage_id, checkDescription, checkPrice, checkStock, checkRanking, async (req, res) => {
+grubposts.put("/:id", async (req, res) => {
     const { id } = req.params;
-    const updatedMenuItem = await updateMenuItem(id, req.body);
-    res.status(200).json(updatedMenuItem);
+    const updatedGrubPost = await updateGrubPost(id, req.body);
+    res.status(200).json(updatedGrubPost);
 
 })
 
 //DELETE
-menuItems.delete("/:id", async (req, res) => {
+grubposts.delete("/:id", async (req, res) => {
     const { id } = req.params;
-    const deletedMenuItem = await deleteMenuItem(id);
+    const deletedGrubPost = await deleteGrubPost(id);
 
-    if (deletedMenuItem.id) {
-        res.status(200).json(deletedMenuItem);
+    if (deletedGrubPost.id) {
+        res.status(200).json(deletedGrubPost);
     } else {
         res.status(404).json("Menu Item not found");
     }
 });
 
-module.exports = menuItems;
+module.exports = grubposts;
